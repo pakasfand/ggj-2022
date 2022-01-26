2dc3dacba4a8c03db3b816619b3dabcb7d6fa855
Asfand <asbutt@ualberta.ca>
2022-01-25 16:45:13 -0800

    Add player prefab
    Add portal prefab
    Rename scripts
    Add level 1
    Add cinemachine

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+	[RequireComponent (typeof (PlayerController))]
+	public class PlayerInput : MonoBehaviour {
+
+		PlayerController player;
+
+		void Start () {
+			player = GetComponent<PlayerController> ();
+		}
+
+		void Update () {
+			Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+			player.SetDirectionalInput (directionalInput);
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				player.OnJumpInputDown ();
+			}
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				player.OnJumpInputUp ();
+			}
+		}
+	}
+}
