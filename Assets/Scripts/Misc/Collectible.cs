ec401c0d87307fca2d27069442ce2cee9a60db06
Ezra Marin <marinezra02@gmail.com>
2022-01-28 23:26:33 -0500

    Added collectibles and modified immutable stacks

---
+++ Assets/Scripts/Misc/Collectible.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Misc;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public CollectibleType type;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
--- Assets/Scripts/Misc/CollectibleManager.cs
+++ Assets/Scripts/Misc/CollectibleManager.cs
@@ -1,17 +1,51 @@
+using UnityEngine;
+
 namespace Misc
 {
+    public enum CollectibleType : byte
+    {
+        MECHANICAL,
+        NATURE
+    }
+
+    [SerializeMonoBehaviour]
     public class CollectibleManager : Singleton<CollectibleManager>
     {
-        public void PushPreCheckpoint()
+        [SerializeMonoBehaviourField] private ImmutableStack<GameObject> mechanicalPreCheckpointStack;
+        [SerializeMonoBehaviourField] private ImmutableStack<GameObject> naturePreCheckpointStack;
+        private ImmutableStack<GameObject> mechanicalPostCheckpointStack;
+        private ImmutableStack<GameObject> naturePostCheckpointStack;
+
+        public void PushPreCheckpoint(GameObject obj)
         {
-            
+            if (obj.GetComponent<Collectible>().type == CollectibleType.NATURE)
+            {
+                naturePreCheckpointStack.Push(obj);
+            }
+            else
+            {
+                mechanicalPostCheckpointStack.Push(obj);
+            }
         }
 
         public void PushPostCheckpoint()
         {
+            while (!naturePreCheckpointStack.IsEmpty)
+            {
+                GameObject tmp;
+                naturePreCheckpointStack = naturePreCheckpointStack.Pop(out tmp);
+                naturePostCheckpointStack = naturePostCheckpointStack.Push(tmp);
+            }
+            
+            while (!mechanicalPreCheckpointStack.IsEmpty)
+            {
+                GameObject tmp;
+                mechanicalPreCheckpointStack = mechanicalPreCheckpointStack.Pop(out tmp);
+                mechanicalPostCheckpointStack = mechanicalPostCheckpointStack.Push(tmp);
+            }
             
         }
-        
+
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
